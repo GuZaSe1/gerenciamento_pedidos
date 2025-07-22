f11ef08d3577f0be65b3b2b25e1c261b95c5b986
Gustavo Z. Segeuca <168004928+GuZaSe1@users.noreply.github.com>
2025-07-22 14:19:03 -0300

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,11 +1,1 @@
 <?php ?>
-</body>
-
-<!-- ALTERAÇÃO 1: Adicionado o container genérico que será usado por todos os dialogs. -->
-<div id="generic-dialog-container"></div>
-
-<!-- ALTERAÇÃO 2: Incluído o nosso novo helper de JavaScript. -->
-<!-- Certifique-se que o caminho para o arquivo está correto. -->
-<script type="text/javascript" src="js/dialog_helper.js"></script>
-
-</html>
