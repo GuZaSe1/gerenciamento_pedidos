8df43ea74c23804bef1129b31937de6df726c4d3
Gustavo Z. Segeuca <168004928+GuZaSe1@users.noreply.github.com>
2025-07-23 17:47:36 -0300

    Update header.php

--- header.php
+++ header.php
@@ -1,13 +1,13 @@
 <?php ?>
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
-    
+
     <?php ?>
     <title><?= isset($pageTitle) ? htmlspecialchars($pageTitle) : 'Sistema de Pedidos' ?></title>
 
-    <?php // Links JS que se repetem em todas as paginas ?>
     <link rel="stylesheet" type="text/css" href="https://www.jeasyui.com/easyui/themes/material-teal/easyui.css">
     <link rel="stylesheet" type="text/css" href="https://www.jeasyui.com/easyui/themes/icon.css">
     <script type="text/javascript" src="https://www.jeasyui.com/easyui/jquery.min.js"></script>
@@ -15,21 +15,28 @@
     <script type="text/javascript" src="https://www.jeasyui.com/easyui/locale/easyui-lang-pt.js"></script>
 
     <style>
-        body { 
+        body {
             font-family: sans-serif;
         }
-        .main-container { 
+
+        .main-container {
             max-width: 800px;
             margin: 20px auto;
             padding: 10px;
         }
-        .form-container { 
+
+        .form-container {
             display: flex;
-            justify-content: center; 
-            align-items: center; 
-            min-height: 90vh; 
-            padding: 20px; 
+            justify-content: center;
+            align-items: center;
+            min-height: 90vh;
+            padding: 20px;
+        }
+
+        .datagrid-row-alt {
+            background-color: #f2f2f2;
         }
     </style>
 </head>
+
 <body>
